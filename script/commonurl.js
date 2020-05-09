82a7ce0c4a1b67b144edb61d531b598eecf6e008
343429803@qq.com <343429803@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2020-05-09 02:32:45 +0000

    设置了全局的聊天地址
    git-svn-id: svn://svn6.apicloud.com/A6031871807996/NailVision@56 0e1a2743-8d03-4e5a-a60c-7e26af42b145

--- script/commonurl.js
+++ script/commonurl.js
@@ -1,12 +1,9 @@
-// var url = 'http://nailvision.tao.li4.cn/api/'   //正式地址
 var url = 'http://ym.s.li4.cn/api/'    //测试地址
-var img_url = 'http://ym.s.li4.cn/'
+var img_url = 'http://ym.s.li4.cn/'    //个别图片后台没有自动匹配AP,需要自行添加
 
-var socketUrl = 'http://ym.s.li4.cn:9501/'
+var socketUrl = 'ws://ym.s.li4.cn:1936'   //聊天的地址，全局设置
 var shareurl = 'http://ym.s.li4.cn/register/'   //前台分享的链接地址
-// var socketUrl = 'http://chat.tao.li4.cn:9501/'
-// var socketUrl = 'http://192.168.124.39:9501/'
-// var url = 'http://192.168.31.14:81/'
+
 
 //提醒用户设置权限 ，不然个别会提醒：请插入SD卡
 function reqPermission(one_per, callback) {
